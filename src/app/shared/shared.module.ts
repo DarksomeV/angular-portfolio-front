d3b5ce307cacf9a293fd6f84da1f79d58652d44b
DarksomeV <viktor.khyshnyak@gmail.com>
2020-03-11 22:03:09 +0200

    added mobile version

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BreakpointService } from './services/breakpoint.service';
 
 
 
@@ -7,6 +8,10 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [],
+  providers: [
+    BreakpointService
   ]
 })
 export class SharedModule { }
