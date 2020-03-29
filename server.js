02aad9097e0df6974dc751c11c4f8d1c284df852
DarksomeV <viktor.khyshnyak@gmail.com>
2020-03-29 23:41:16 +0300

    old version

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const compression = require('compression');
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
 const locale = require('locale');
-const supportedLocales = ['en', 'ru'];
 const app = express();
-const port = process.env.PORT || 8080;
-// Gzip
-app.use(compression());
-// Serve static files from the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Detect locale and determine best match
+const supportedLocales = ['en', 'ru'];
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/portfolio'));
 app.use(locale(supportedLocales));
-// Start the app by listening on the default Heroku port
-app.listen(port);
-// Return index.html for all GET requests for PathLocationStrategy
-// And accept locale style URLs: /en/example
-app.get('/*', (req, res) => {
+app.get('/*', function(req,res) {
   const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
   const locale = matches && supportedLocales.indexOf(matches[1]) !== -1 ? matches[1] : req.locale;
-  res.sendFile(path.join(`${__dirname}/dist/index.html`));
+  res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
 });
-console.log(`Server listening on ${port}`);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
