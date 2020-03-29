208d02bd7e001bc5490e7b77a4178176134d1927
DarksomeV <viktor.khyshnyak@gmail.com>
2020-03-30 00:08:52 +0300

    last try

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const locale = require('locale');
+
 const app = express();
-const supportedLocales = ['en', 'ru'];
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/portfolio'));
-app.use(locale(supportedLocales));
+
 app.get('/*', function(req,res) {
-  const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
-  const locale = matches && supportedLocales.indexOf(matches[1]) !== -1 ? matches[1] : req.locale;
+
   res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
 });
 
